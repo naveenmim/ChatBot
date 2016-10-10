9a2ec405513f93ddb35833721fe2ca9392538972
naveenmim <navkyz@gmail.com>
2016-10-10 12:56:04 +0530

    Test

---
+++ ChatBot/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ChatBot.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ ChatBot/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatBot.Startup))]
+namespace ChatBot
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
